ca1fa7f85c21b0a0a7632cb9a0e19296595b4a3f
bala-amypo <bala@amypo.in>
2025-12-26 08:21:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/TierUpgradeRuleRepository.java
+++ src/main/java/com/example/demo/repository/TierUpgradeRuleRepository.java
@@ -3,7 +3,7 @@ import com.example.demo.entity.TierUpgradeRule;
 import java.util.List;
 import java.util.Optional;
 
-public interface TierUpgradeRuleService {
+public interface TierUpgradeRuleService { 
     TierUpgradeRule createRule(TierUpgradeRule r);
     TierUpgradeRule updateRule(Long id, TierUpgradeRule r);
     List<TierUpgradeRule> getActiveRules();
