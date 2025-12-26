daaed3dc819c1199f0c9252deced25af4cbb51d2
bala-amypo <bala@amypo.in>
2025-12-26 15:28:04 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,16 +1,21 @@
-// package com.example.demo.servlet;
+package com.example.demo.servlet;
 
-// import jakarta.servlet.annotation.WebServlet;
-// import jakarta.servlet.http.HttpServlet;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import java.io.IOException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-// @WebServlet(urlPatterns = "/status")
-// public class SimpleStatusServlet extends HttpServlet {
-//     @Override
-//     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//         resp.setStatus(200);
-//         resp.getWriter().write("Customer Loyalty Tier Upgrader is running");
-//     }
-// }
+import java.io.IOException;
+
+@WebServlet("/status")
+public class SimpleStatusServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp)
+            throws IOException {
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Application is running");
+    }
+}
